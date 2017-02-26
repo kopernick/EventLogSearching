0e76c2d2547af3f58b5f421598ed8d6a356f0fc4
Preecha <kopernick@live.com>
2017-02-26 22:44:18 +0700

    Add dynamic Version

--- EventLogSearching/Service/About.xaml.cs
+++ EventLogSearching/Service/About.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Deployment.Application;
 
 namespace EventLogSearching.Service
 {
@@ -19,9 +20,25 @@ namespace EventLogSearching.Service
     /// </summary>
     public partial class About : Window
     {
+        public string version { get; set; }
         public About()
         {
             InitializeComponent();
+            DataContext = this;
+            version = null;
+            try
+            {
+                //// get deployment version
+                version = "Version" + ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString();
+                txtVersion.Content = version;
+            }
+            catch (InvalidDeploymentException)
+            {
+                //// you cannot read publish version when app isn't installed 
+                //// (e.g. during debug)
+                version = "not installed";
+                txtVersion.Content = version;
+            }
         }
     }
 }
